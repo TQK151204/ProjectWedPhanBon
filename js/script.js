133b3f86a448fd902adbc695037e0898bb61c1f2
TQK151204 <22130121@st.hcmuaf.edu.vn>
2024-11-13 13:33:55 +0700

    Home and Product

--- js/script.js
+++ js/script.js
@@ -1,10 +1,36 @@
-document.getElementById("account-icon").addEventListener("click", function (event) {
-    event.stopPropagation();
-    const dropdown = document.querySelector(".account-dropdown");
-    dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
-});
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navabar');
+menu.onclick =() =>{
+    menu.classList.toggle('fa-time');
+    navbar.classList.toggle('active');
+}
 
-// Đóng menu khi nhấp bên ngoài
-document.addEventListener("click", function () {
-    document.querySelector(".account-dropdown").style.display = "none";
-});
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+}
+document.querySelector('#search-icon').onclick = () =>{
+    document.querySelector('#search-form').classList.toggle('active');
+}
+document.querySelector('#close').onclick = () =>{
+    document.querySelector('#search-form').classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+
+    
+  });
+
+  
